a737f36ce354ecfacf8c2ce2f7dd3491dd2a1485
Goldwyn Rodrigues <rgoldwyn@suse.com>
2023-01-27 14:06:19 -0600

    btrfs: use IOMAP_NOWAIT for nowait actions

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -1005,7 +1005,7 @@ static int btrfs_buffered_iomap_begin(struct inode *inode, loff_t pos,
 	size_t sector_offset = pos & (fs_info->sectorsize - 1);
 	size_t len, write_bytes = length;
 	struct btrfs_iomap *bi;
-	bool nowait = false;
+	bool nowait = iomap->flags & IOMAP_NOWAIT;
 
 	bi = kzalloc(sizeof(struct btrfs_iomap), GFP_NOFS);
 	if (!bi)
